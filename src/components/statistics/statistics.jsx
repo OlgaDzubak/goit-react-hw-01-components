8e4b29f6dab1e9bc9610c7f366038a5ffd0cecb2
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-05-29 12:36:59 +0300

    Update statistics.jsx

--- src/components/statistics/statistics.jsx
+++ src/components/statistics/statistics.jsx
@@ -7,7 +7,7 @@ export const Statistics =({stats, title}) => {
     return (
         <section className={css.statistics}>
             { 
-            {title} && (<h2 className={css.title}>{title}</h2>)
+            (title) && (<h2 className={css.title}>{title}</h2>)
             }
             <ul className={css.statlist}>
                 {
